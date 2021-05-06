6aa739fcfee52bb83657f5a1ce4dd6ce1833afe3
kadiri <kadiri2006@gmail.com>
2021-05-06 20:52:25 +0530

    create class component with constructor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
-let App = () => {
-  return <h1>testing ok</h1>;
-};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "testing" };
+  }
+  render() {
+    return <h1>{this.state.title}</h1>;
+  }
+}
+// let App = () => {
+//   return <h1>testing ok</h1>;
+// };
+
 export default App;
