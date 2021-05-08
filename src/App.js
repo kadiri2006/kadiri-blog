d3de0c317580d498f83ed9d7996019e6a5215a5e
kadiri <kadiri2006@gmail.com>
2021-05-08 08:09:22 +0530

    my toggle code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
 import "./assets/css/style.css";
-let App = () => {
-  return <section className="box">testing ok..</section>;
-};
+// let App = () => {
+//   return <div className="box">testing ok..</div>;
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { display: false };
+    this.afterClick = this.afterClick.bind(this);
+  }
+  afterClick() {
+    this.setState((state) => ({
+      display: !this.state.display,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="box">
+          <h1>iam testing</h1>
+        </div>
+        <div className="btn">
+          <button type="button" onClick={this.afterClick}>
+            {this.state.display ? "button on" : null};
+          </button>
+        </div>
+        <div>{this.state.display ? <img
+  className="image"
+  src="https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
+/>: null};</div>
+      </div>
+    );
+  }
+}
 
 export default App;
+
+//  <img
+//   className="image"
+//   src="https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
+// />
