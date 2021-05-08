c0fc8a5b65bdfa5847f4b66c4873e53e65621346
kadiri <kadiri2006@gmail.com>
2021-05-08 08:52:05 +0530

    this is perfect toggle code

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { display: false };
-    this.afterClick = this.afterClick.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
   }
-  afterClick() {
-    this.setState((state) => ({
-      display: !this.state.display,
-    }));
+  clickHandler() {
+    this.setState({ display: !this.state.display });
   }
-
   render() {
     return (
       <div>
@@ -23,14 +20,16 @@ class App extends React.Component {
           <h1>iam testing</h1>
         </div>
         <div className="btn">
-          <button type="button" onClick={this.afterClick}>
-            {this.state.display ? "button on" : null};
+          <button type="button" onClick={this.clickHandler}>
+            button
           </button>
         </div>
-        <div>{this.state.display ? <img
-  className="image"
-  src="https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
-/>: null};</div>
+        {this.state.display ? (
+          <img
+            className="image"
+            src="https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
+          />
+        ) : null}
       </div>
     );
   }
