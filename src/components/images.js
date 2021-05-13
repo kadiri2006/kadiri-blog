494d05b33e5ab30760687ce1e91c7e806f2688a2
kadiri <kadiri2006@gmail.com>
2021-05-13 16:36:30 +0530

    cleared all problems

--- src/components/images.js
+++ src/components/images.js
@@ -7,26 +7,36 @@ export default function Images() {
     "https://images.unsplash.com/photo-1620246499650-86ee085562fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80",
     "https://images.unsplash.com/photo-1617932631696-f36237bedf05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
   ]);
-  const [urlAdd, seturlAdd] = useState(null);
+  const [urlAdd, seturlAdd] = useState(photos); 
   const [enterValue, setenterValue] = useState("");
-
+  const [mouseE, setmouseE] = useState("");
   function ShowImages() {
-    return photos.map((img) => {
-      return <img src={img} className="indiPhotos" />;
+    return photos.map((img, index) => {
+      return (
+        <img
+          src={img}
+          onClick={() => handleClick(index)}
+          onMouseEnter={() => handleMouseEnter(index)}
+          className="indiPhotos"
+          key={index}
+        />
+      );
     });
   }
 
-  useEffect(() => {
-    let my = setInterval(() => {}, 1000);
+  let handleClick = (index) => {
+    let x = [...photos];
+    setphotos(x.filter((x, index) => index != mouseE));
+    seturlAdd(x.filter((x, index) => index != mouseE));
+  };
 
-    return () => {
-      clearInterval(my);
-    };
-  });
+  let handleMouseEnter = (index) => {
+    setmouseE(index);
+  };
 
-  let imageAdd = () => {
+  let handleClick2 = () => {
     setenterValue("");
-
+    console.log([...urlAdd]);
     setphotos([...urlAdd]);
   };
 
@@ -40,9 +50,8 @@ export default function Images() {
       <ShowImages />
       <div className="ib">
         <input type="text" onChange={handleChange} value={enterValue} />
-        <button onClick={imageAdd}>add url to images</button>
+        <button onClick={handleClick2}>add url to images</button>
       </div>
     </div>
   );
 }
-
