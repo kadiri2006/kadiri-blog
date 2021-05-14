c5da9d244a43f5d6e504f1561e68f5362a5d4bfd
kadiri <kadiri2006@gmail.com>
2021-05-14 20:15:19 +0530

    offcourse some final gallery

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ export default function App() {
   const testd = useRef(false);
 
   useEffect(() => {
-    console.log("App mount");
+    
   }, []);
 
   useEffect(() => {
     if (testd.current) {
-      console.log("App updated");
+     
     } else {
       testd.current = true;
     }
@@ -23,7 +23,7 @@ export default function App() {
 
   return (
     <div>
-      {console.log("App render")}
+      
       <h1 className="heading">Collection of Images</h1>
       <div>
         <button onClick={clickHandler} className="btn">click me</button>
